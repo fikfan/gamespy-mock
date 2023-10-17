bde9aa92256ecf3b857d9b438e148ccea2aa96be
Fikri <fikri@hokantan.com>
2023-10-17 10:21:51 +0800

    gamespy react init

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center fixed bottom-0 w-full">
+            <p className="text-center">Footer Stuff</p>
+        </footer>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+      },
+      textColor: {
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+      },
+      borderColor: {
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)'
+      }
     },
   },
   plugins: [],
